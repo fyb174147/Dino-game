aabe3b6039096481ab83731a67e7e65f1ea19720
maomaoisgoodcat3 <162585693+maomaoisgoodcat3@users.noreply.github.com>
2024-03-13 22:01:37 +0700

    Add files via upload

---
+++ dino.cpp
@@ -0,0 +1,78 @@
+﻿#include "dino.h"
+
+Dino::Dino() {
+	jumping = false;
+	falling = false;
+
+	posX = 100;
+	posY = ground;
+
+	deltaH = 0;
+
+	timeJump = 0; 
+	maxH = 200; 
+
+	status = 0;// 0 là chạy hoặc nhảy , 1 là cúi đầu và 2 là hẹo nhé 
+}
+
+void Dino::handleEvent(SDL_Event& e) {
+	if (e.type == SDL_KEYDOWN and e.key.keysym.sym == SDLK_SPACE) {
+		if (timeJump < 2 and !jumping) {
+			jumping = true;
+			deltaH = 0;
+			timeJump++;
+		}
+	}
+	if (e.type == SDL_KEYDOWN and e.key.keysym.sym == SDLK_s) {
+		if (status != 2) status = 1;
+	}
+	if (e.type == SDL_KEYUP and status != 2) {
+		status = 0;
+	}
+	if (e.type == SDL_KEYDOWN and e.key.keysym.sym == SDLK_0) {
+		status = 2;
+	}
+	
+
+}
+
+void Dino::move() {
+	if (jumping) {
+		posY -= jump_speed;
+		deltaH += jump_speed;
+		if (deltaH >= maxH) {
+			jumping = false;
+			falling = true;
+		}
+		
+	}
+	else if (falling) {
+
+		posY += fall_speed;
+		if (posY >= ground ) {
+			falling = false;
+			posY = ground;
+		}
+	}
+	if (posY == ground) {
+		timeJump = 0;
+		deltaH = 0;
+	}
+}
+
+void Dino::render(SDL_Rect* clips, SDL_Renderer* gRenderer,LTexture characterTexture) {
+
+	characterTexture.Render(posX, posY, gRenderer, clips);
+}
+
+
+double Dino::getposX() {
+	return posX;
+}
+double Dino::getposY() {
+	return posY;
+}
+
+int Dino::getStatus() {
+	return status;
+}
---
+++ dino.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include<SDL.h>
+#include<SDL_image.h>
+#include<iostream>
+#include "LTexture.h"
+class Dino {
+public:
+	int ground = 500;
+	double jump_speed = 0.5; //chịu khó chỉnh tay nhé, vì hiện tại tốc độ rơi của dino chưa tính tới việc tăng hay giảm gì cả 
+	double fall_speed = 0.5;
+
+	Dino();
+
+	void handleEvent(SDL_Event& e);
+
+	void move();
+
+	void render(SDL_Rect* currentClip, SDL_Renderer* gRenderer,LTexture characterTexture);
+
+
+	double getposX();
+
+	double getposY();
+
+	int getStatus();
+
+private:
+	bool jumping;
+	bool falling;
+
+	double posX;// nhớ là double đấy, k chỉnh sang int đâu. Lưu ý: khi dùng ltexture::render(int x,int y,...) thif convert đám posX posY sang int. kiểu x = int(x) ý:D 
+	double posY;
+
+	double deltaH;
+
+	int timeJump; //số lần nhảy 
+	double maxH; //độ cao cao nhất chạm tới 
+
+	int status;
+	
+};
